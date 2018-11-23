1891a918b4e47e05f918eefeea03d3e40f794346
Tommy van Aalst <tommy.van.aalst@gmail.com>
2018-11-23 21:46:32 +0100

    Added get user by ID and started on register form

--- src/main/java/com/spe/example/demo/UserRepository.java
+++ src/main/java/com/spe/example/demo/UserRepository.java
@@ -1,6 +1,8 @@
 package com.spe.example.demo;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.spe.example.demo.User;
 
@@ -8,5 +10,8 @@ import com.spe.example.demo.User;
 // CRUD refers Create, Read, Update, Delete
 
 public interface UserRepository extends CrudRepository<User, Integer> {
+
+    @Query("SELECT u FROM User u WHERE u.id = :id")
+    public User findById(@Param("id") Long id);
 
 }
